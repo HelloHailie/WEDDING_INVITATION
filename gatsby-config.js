b8bd278173b6f3f4d2e8b898b27e9e660c2c055c
HelloHailie <alsltjsals@gmail.com>
2025-02-09 23:17:26 +0900

    feat: favicon 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,24 @@ module.exports = {
     title: `weddingInvitation`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-favicons",
+      options: {
+        logo: "./src/images/favicon.ico",
+        appName: "weddingInvitation",
+        background: "#fff",
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          yandex: false,
+          windows: true,
+        },
+      },
+    },
+  ],
 };
