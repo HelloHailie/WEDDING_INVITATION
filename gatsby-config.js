a0944c6acdfa57e5851779862ecdfd834240d54d
HelloHailie <alsltjsals@gmail.com>
2025-02-09 23:09:21 +0900

    fix: build 에러 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `weddingInvitation`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Wedding Invitation`,
-        short_name: `Wedding`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffffff`,
-        display: `standalone`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
-      },
-    },
-  ]
+  plugins: ["gatsby-plugin-styled-components"],
 };
