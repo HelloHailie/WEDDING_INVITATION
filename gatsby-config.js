24bc569e1e2a89fb10b1cb51f4d394c328324251
HelloHailie <alsltjsals@gmail.com>
2025-02-09 23:01:47 +0900

    feat: favicon 이미지 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,19 @@ module.exports = {
     title: `weddingInvitation`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Wedding Invitation`,
+        short_name: `Wedding`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `standalone`,
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+      },
+    },
+  ]
 };
